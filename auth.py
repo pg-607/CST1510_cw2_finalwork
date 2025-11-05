181b8cc73bc7f4cdd018b8497ff9cb5701847443
pg-607 <PG607@live.mdx.ac.uk>
2025-11-05 11:20:41 +0400

    Initial commit

---
+++ auth.py
@@ -0,0 +1,17 @@
+import bcrypt
+import os
+
+def hash_password(plain_text_password):
+   
+    # TODO: Encode the password to bytes (bcrypt requires byte strings)
+    
+    
+    # TODO: Generate a salt using bcrypt.gensalt()
+    
+    
+    # TODO: Hash the password using bcrypt.hashpw()
+    
+    
+    # TODO: Decode the hash back to a string to store in a text file
+    
+    return 
